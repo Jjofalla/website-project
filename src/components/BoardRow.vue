7273247d6d630eccc845a7069e21df24bf31232d
Jjofalla <jamiefalla@me.com>
2023-12-17 18:34:11 +0000

    Improve Character Input
    Event handling for keydown

--- src/components/BoardRow.vue
+++ src/components/BoardRow.vue
@@ -8,25 +8,52 @@ import Tile from './BoardTile.vue';
 const number_of_tiles = 4;
 const target_idx = ref(0);
 const chars = ref(['', '', '', ''])
+const handled = ref(new Set());
 
-function add_char(event) {
+function handleKeyDown(event) {
+    if (handled.value.has(event.key)) {
+        return;
+    }
+    handled.value.add(event.key);
+
     if (target_idx.value < number_of_tiles && /^[a-zA-Z]$/.test(event.key)) {
-        chars.value[target_idx.value++] = event.key;
-    }
-};
+        chars.value[target_idx.value] = event.key;
+        target_idx.value++;
 
-function remove_char() {
-    if (target_idx.value > 0) {
-        chars.value[--target_idx.value] = '';
+    } else if (event.key === 'Delete' || event.key === 'Backspace') {
+        if (target_idx.value > 0) {
+            target_idx.value--;
+            chars.value[target_idx.value] = '';
+        }
+
+    } else if (event.key === 'Enter') {
+        ;
     }
-};
+
+}
+
+function handleKeyUp(event) {
+    handled.value.delete(event.key);
+}
+
+// function add_char(event) {
+//     if (target_idx.value < number_of_tiles && /^[a-zA-Z]$/.test(event.key)) {
+//         chars.value[target_idx.value++] = event.key;
+//     }
+// };
+
+// function remove_char() {
+//     if (target_idx.value > 0) {
+//         chars.value[--target_idx.value] = '';
+//     }
+// };
 
 const is_highlight = (key) => key === target_idx.value;
 
 </script>
 
 <template>
-    <div class="row" tabindex="0" @keydown="add_char($event)" @keydown.delete="remove_char()" @keydown.enter="submit_guess()">
+    <div class="row" tabindex="0" @keydown="handleKeyDown($event)" @keyup="handleKeyUp($event)">
         <Tile v-for="tile in number_of_tiles" :key="tile" :highlight="is_highlight(tile - 1)" :char="chars[tile - 1]"/>
     </div>
 </template>
